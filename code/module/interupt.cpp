1f62dc53469f04f6f261d7b15c0f7ed3b30f98a2
Nuur-R <firdausnuurr@gmail.com>
2022-12-06 11:05:08 +0700

    interupt button fix

--- code/module/interupt.cpp
+++ code/module/interupt.cpp
@@ -20,12 +20,17 @@
 #include <ACS712.h>
 
 #define cpmPin D7
-int count = 0;
+
 bool bstat = false;
-void ICACHE_RAM_ATTR impulseCount(void)
+void impulseCount(void)
 {
   bstat = true;
 }
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1    // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 void setup()
 {
   Serial.begin(115200);
@@ -33,8 +38,17 @@ void setup()
   pinMode(cpmPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(cpmPin), impulseCount, CHANGE);
   Serial.println("End Setup");
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Setup");
+  display.display();
+  delay(1500);
 }
 
+int count = 0;
 void loop()
 {
   if (bstat == true)
@@ -42,7 +56,13 @@ void loop()
     count++;
     Serial.print("Count: ");
     Serial.println(count);
-    delay(500);
+    delay(200);
     bstat = false;
   }
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Count: ");
+  display.println(count);
+  display.display();
+  
 }
