297a1e10cc3854dfe526473f1b73275fc60609d4
nuur_r <firdausnuurr@gmail.com>
2022-12-22 12:24:51 +0700

    Thermocouple PZEM Integrasi udah ngirim ke Odoo

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+// Node02
+// Oven
+
 #include <Arduino.h>
 
 #include <SPI.h>
@@ -15,206 +18,68 @@
 #include <DHT.h>
 #include <DHT_U.h>
 #include <ACS712.h>
+#include <max6675.h>
+#include <PZEM004Tv30.h>
+#include <SoftwareSerial.h>
 
 const char *serverName = "http://103.172.204.18:8069/jsonrpc";
 
 unsigned long lastTime = 0;
-unsigned long timerDelay = 60000 * 30;
+unsigned long timerDelay = 10000;
 unsigned long displayTime = 2000;
 
-#define BUZZPIN D8
-#define DHTPIN D4
-#define DHTTYPE DHT22
+#define BUZZPIN D4
+int thermoSO = D6;
+int thermoCS = D5;
+int thermoCSK = D0;
 
-#define cpmPin D7
-volatile bool bstat = false;
-
-DHT_Unified dht(DHTPIN, DHTTYPE);
-uint32_t delayMS;
+MAX6675 thermocouple(thermoCSK, thermoCS, thermoSO);
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET -1    // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-void buzz(int pin, int delayTime, int repeat);
-void sendData(float temperatur,
-              float kelembapan,
-              float arus,
-              float biaya_listrik,
-              int cycle,
-              float daya,
-              String lokasi,
-              String nama_mesin,
-              int pegawai_id,
-              bool status_power,
-              float tegangan,
-              float total_produksi,
-              float volume,
-              int oven_id);
-void startDisplay();
-void wifiConnectedDisplay();
-void mainDisplay(String status, float temperatur, float kelembapan, float arus, float daya, int cycle);
 
-void ICACHE_RAM_ATTR impulseCount(void)
-{
-  bstat = true;
-}
-
-String lokasi = "Ruang Gudang";
-String node = "Node01";
-String nama_mesin = "Gudang";
+String lokasi = "Ruang Tester";
+String node = "NodeXX";
+String nama_mesin = "Mesin Test";
 String nodeName = node + "-" + nama_mesin;
 String accessPointIP = "192.168.4.1";
 
 int bahan_id = 1;
+int oven_id = 4;
 
 float suhu_min = 24;
 float suhu_max = 37;
-float temperatur = 0;
+float temperatur_oven = 0;
 float kelembapan = 0;
 
-float tegangan = 220;
+float tegangan = 0;
 float arus = 0;
 float daya = arus * tegangan;
 float biaya_listrik = 0;
 bool status_power = true;
 
+String t_on = "2020-01-01 00:00:00";
+String t_off = "2020-01-01 00:00:00";
+
 int cycle = 1;
 int pegawai_id = 7;
-int oven_id = 1;
 
 float total_produksi = 1230;
 float volume = 100;
 
 DynamicJsonDocument doc(1024);
-ACS712 ACS(A0, 5.0, 1023, 100);
 
-void setup()
-{
-  Serial.begin(115200);
-  while (!Serial)
-    ;
-  WiFiManager wifiManager;
-  wifiManager.setBreakAfterConfig(true);
+#if !defined(PZEM_RX_PIN) && !defined(PZEM_TX_PIN)
+#define PZEM_RX_PIN D8
+#define PZEM_TX_PIN D7
+#endif
 
-  pinMode(BUZZPIN, OUTPUT);
-  digitalWrite(BUZZPIN, LOW);
+SoftwareSerial pzemSWSerial(PZEM_RX_PIN, PZEM_TX_PIN);
+PZEM004Tv30 pzem(pzemSWSerial);
 
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  startDisplay();
-
-  if (!wifiManager.autoConnect(nodeName.c_str()))
-  {
-    Serial.println("failed to connect, we should reset as see if it connects");
-    delay(3000);
-    ESP.reset();
-    delay(5000);
-  }
-  Serial.println("connected...yeey :)");
-  Serial.println("local ip");
-  wifiConnectedDisplay();
-  buzz(BUZZPIN, 700, 3);
-
-  dht.begin();
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  dht.humidity().getSensor(&sensor);
-
-  Serial.println(__FILE__);
-  Serial.print("ACS712_LIB_VERSION: ");
-  Serial.println(ACS712_LIB_VERSION);
-
-  ACS.autoMidPoint();
-  Serial.print("MidPoint: ");
-  Serial.print(ACS.getMidPoint());
-  Serial.print(". Noise mV: ");
-  Serial.println(ACS.getNoisemV());
-  delayMS = sensor.min_delay / 1000;
-
-  // set LIMIT_SWITCH as input
-  pinMode(cpmPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(cpmPin), impulseCount, CHANGE);
-}
-
-void loop()
-{
-  delay(delayMS);
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature))
-  {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else
-  {
-    temperatur = event.temperature;
-  }
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity))
-  {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else
-  {
-    kelembapan = event.relative_humidity;
-  }
-
-  float mA = ACS.mA_AC();
-  Serial.println(mA);
-  if (bstat == true)
-  {
-    cycle++;
-    delay(300);
-    bstat = false;
-  }
-
-  // arus mA ke ampare
-  arus = mA / 1000;
-  // menghitung daya listrik dari Mili Ampare dan tegangan
-  daya = arus * tegangan;
-  // menghitung biaya listrik dari daya listrik dan waktu
-  biaya_listrik = daya * 0.000001 * 15;
-  // menghitung total produksi dari volume dan cycle
-  total_produksi = volume * cycle;
-  // set status_power true jika arus lebih dari 0.23
-  status_power = arus >= 0.05 ? true : false;
-
-  if (temperatur <= suhu_min)
-  {
-    mainDisplay("Dingin", temperatur, kelembapan, arus, daya, cycle);
-    buzz(BUZZPIN, 1000, 3);
-  }
-  else if (temperatur >= suhu_max)
-  {
-    mainDisplay("Panas", temperatur, kelembapan, arus, daya, cycle);
-    buzz(BUZZPIN, 300, 5);
-  }
-  else
-  {
-    mainDisplay("Normal", temperatur, kelembapan, arus, daya, cycle);
-  }
-
-  if (millis() - lastTime > timerDelay)
-  {
-    lastTime = millis();
-    Serial.println("Publishing sensor values...");
-    sendData(temperatur,
-             kelembapan,
-             arus,
-             biaya_listrik,
-             cycle,
-             daya,
-             lokasi,
-             nama_mesin,
-             pegawai_id,
-             status_power,
-             tegangan,
-             total_produksi,
-             volume,
-             oven_id);
-  }
-}
 
 void buzz(int pin, int delayTime, int repeat)
 {
@@ -237,7 +102,7 @@ void startDisplay()
   display.println("Start...");
   display.setTextSize(1);
   display.setCursor(10, 30);
-  display.println(nodeName.c_str());
+  display.println(nama_mesin);
   display.setCursor(10, 40);
   display.println(accessPointIP.c_str());
   display.display();
@@ -255,7 +120,7 @@ void wifiConnectedDisplay()
   display.println(WiFi.localIP());
   display.display();
 }
-void mainDisplay(String status, float temperatur, float kelembapan, float arus, float daya, int cycle)
+void mainDisplay(String status, float temperatur, float tegangan, float arus, float daya)
 {
   display.clearDisplay();
   display.setTextSize(2);
@@ -264,8 +129,13 @@ void mainDisplay(String status, float temperatur, float kelembapan, float arus, 
   display.println(status);
   display.setTextSize(1);
   display.setCursor(0, 20);
-  display.println("Suhu       : " + String(temperatur) + " C");
-  display.println("Kelembapan : " + String(kelembapan) + " %");
+  display.println("Suhu     : " + String(temperatur) + " C");
+  display.setCursor(0, 30);
+  display.println("Tegangan : " + String(tegangan) + " V");
+  display.setCursor(0, 40);
+  display.println("Arus     : " + String(arus) + " A");
+  display.setCursor(0, 50);
+  display.println("Daya     : " + String(daya) + " W");
   display.display();
 }
 
@@ -276,44 +146,25 @@ void sendData(float temperatur,
               int cycle,
               float daya,
               String lokasi,
-              String nama_mesin,
+              int oven_id,
               int pegawai_id,
               bool status_power,
+              String t_off,
+              String t_on,
               float tegangan,
               float total_produksi,
-              float volume,
-              int oven_id)
+              float volume)
 {
   if (WiFi.status() == WL_CONNECTED)
   {
-    WiFiClient client;
     HTTPClient http;
+    WiFiClient client;
 
     http.begin(client, serverName);
-
-    doc["jsonrpc"] = "2.0";
-    doc["method"] = "call";
-    doc["params"]["service"] = "object";
-    doc["params"]["method"] = "execute_kw";
-    doc["params"]["args"][0] = "new_spu";
-    doc["params"]["args"][1] = 44;
-    doc["params"]["args"][2] = "12341234";
-    doc["params"]["args"][3] = "kedaireka.iot";
-    doc["params"]["args"][4] = "create";
-    doc["params"]["args"][5][0]["temperatur"] = temperatur;
-    doc["params"]["args"][5][0]["kelembapan"] = kelembapan;
-
-    doc["id"] = 1;
-    String json;
-    serializeJson(doc, json);
-
     http.addHeader("Content-Type", "application/json");
-    int httpResponseCode = http.POST(doc.as<String>());
-
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
+    int httpCode = http.POST("{\"jsonrpc\": \"2.0\",\"method\": \"call\",\"params\": {\"service\": \"object\",\"method\": \"execute_kw\",\"args\": [\"new_spu\",44,\"12341234\",\"kedaireka.mesin.monitoring\",\"create\",[{\"temperatur\": " + String(temperatur_oven) + ",\"kelembapan\": " + String(kelembapan) + ",\"arus\": " + String(arus) + ",\"biaya_listrik\": " + String(biaya_listrik) + ",\"cycle\": " + String(cycle) + ",\"daya\": " + String(daya) + ",\"lokasi\": \"kedai reka\",\"oven_id\": " + String(oven_id) + ",\"pegawai_id\": " + String(pegawai_id) + ",\"status_power\": " + String(status_power) + ",\"t_off\": \"2022-11-25 11:17:39\",\"t_on\": \"2022-11-25 10:35:39\",\"tegangan\": " + String(tegangan) + ",\"total_produksi\": " + String(total_produksi) + ",\"volume\": " + String(volume) + "}]]},\"id\": 1}");
+    Serial.println(httpCode);
     String payload = http.getString();
-    Serial.println("Response: ");
     Serial.println(payload);
 
     display.clearDisplay();
@@ -323,6 +174,7 @@ void sendData(float temperatur,
     display.println("Send Data");
     display.display();
     delay(displayTime);
+
     http.end();
   }
   else
@@ -336,5 +188,75 @@ void sendData(float temperatur,
     display.display();
     delay(displayTime);
   }
-  lastTime = millis();
 }
+
+// =    =    =   =  =  =
+// = = ==   ===  =  == =
+// =  = =  =  =  =  = ==
+
+void setup()
+{
+  Serial.begin(115200);
+  WiFiManager wifiManager;
+  wifiManager.setBreakAfterConfig(true);
+
+  pinMode(BUZZPIN, OUTPUT);
+  digitalWrite(BUZZPIN, LOW);
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  startDisplay();
+
+  buzz(BUZZPIN, 1500, 1);
+  delay(500);
+
+  if (!wifiManager.autoConnect(nodeName.c_str()))
+  {
+    Serial.println("failed to connect, we should reset as see if it connects");
+    delay(3000);
+    ESP.reset();
+    delay(5000);
+  }
+
+  Serial.println("connected...yeey :)");
+  Serial.println("local ip");
+  wifiConnectedDisplay();
+  buzz(BUZZPIN, 700, 3);
+  Serial.println(WiFi.localIP());
+  Serial.println("Timer set to 20 seconds (timerDelay variable), it will take 20 seconds before publishing the first reading.");
+}
+
+void loop()
+{
+  delay(100);
+
+  temperatur_oven = thermocouple.readCelsius();
+  float tegangaan = pzem.voltage();
+  float arus = pzem.current();
+  float daya = pzem.power();
+  float biaya_listrik = pzem.energy();
+  bool status_power = arus > 0.1 ? true : false;
+  mainDisplay("Oven 1", temperatur_oven, tegangaan, arus, daya);
+
+  if (millis() - lastTime > timerDelay)
+  {
+    lastTime = millis();
+    sendData(temperatur_oven,
+             kelembapan,
+             arus,
+             biaya_listrik,
+             cycle,
+             daya,
+             lokasi,
+             oven_id,
+             pegawai_id,
+             status_power,
+             t_off,
+             t_on,
+             tegangaan,
+             total_produksi,
+             volume);
+    
+  }
+}
+
